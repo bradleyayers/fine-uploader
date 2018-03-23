605d6a8ef868cabc77d00aeaca2413464105c4fa
Bradley Ayers <bradley.ayers@gmail.com>
2018-03-23 13:07:15 +1100

    refactor: require native Promise implementation
    BREAKING CHANGE: An error is thrown on load if a native promise implementation is not available.

--- client/js/promise.js
+++ client/js/promise.js
@@ -1,5 +1,9 @@
 /*globals qq*/
 
+if (typeof Promise === "undefined") {
+    throw new Error("FileUploader requires a native promise implementation.");
+}
+
 // Is the passed object a promise instance?
 qq.isGenericPromise = function(maybePromise) {
     "use strict";
