acaf7f3a3ae10569204e1c2721e20b54bdbb32d3
Bradley Ayers <bradley.ayers@gmail.com>
2018-03-26 18:59:09 +1100

    refactor: qq.s3.AbortMultipartAjaxRequester getHeaders to use Promise

--- client/js/s3/multipart.abort.ajax.requester.js
+++ client/js/s3/multipart.abort.ajax.requester.js
@@ -34,26 +34,36 @@ qq.s3.AbortMultipartAjaxRequester = function(o) {
 
     /**
      * Attach all required headers (including Authorization) to the "Abort" request.  This is a promissory function
-     * that will fulfill the associated promise once all headers have been attached or when an error has occurred that
+     * that will resolve the associated promise once all headers have been attached or when an error has occurred that
      * prevents headers from being attached.
      *
      * @param id Associated file ID
      * @param uploadId ID of the associated upload, according to AWS
-     * @returns {qq.Promise}
+     * @returns {Promise}
      */
     function getHeaders(id, uploadId) {
         var headers = {},
-            promise = new qq.Promise(),
             bucket = options.getBucket(id),
             host = options.getHost(id),
             signatureConstructor = getSignatureAjaxRequester.constructStringToSign
                 (getSignatureAjaxRequester.REQUEST_TYPE.MULTIPART_ABORT, bucket, host, options.getKey(id))
                 .withUploadId(uploadId);
 
-        // Ask the local server to sign the request.  Use this signature to form the Authorization header.
-        getSignatureAjaxRequester.getSignature(id, {signatureConstructor: signatureConstructor}).then(promise.success, promise.failure);
-
-        return promise;
+        return new Promise(function(resolve, reject) {
+            // Ask the local server to sign the request.  Use this signature to form the Authorization header.
+            getSignatureAjaxRequester.getSignature(id, {signatureConstructor: signatureConstructor}).then(
+                function (signedPolicy, updatedAccessKey, updatedSessionToken) {
+                    resolve({
+                        signedPolicy: signedPolicy,
+                        updatedAccessKey: updatedAccessKey,
+                        updatedSessionToken: updatedSessionToken
+                    });
+                },
+                function (reason) {
+                    reject(new Error(reason));
+                }
+            );
+        });
     }
 
     /**
